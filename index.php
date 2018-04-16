0f8549a9437e1b58b75ce5eee37175d76251eab8
Charles <cosmasndu@gmail.com>
2018-04-16 15:20:09 +0100

    Index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HNG Internship Stage 1</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='https://fonts.googleapis.com/css?family=Philosopher' rel='stylesheet'>
+</head>
+<body>
+<h1> Actually,Figma is cool...</h1>
+<h4 class="head-text">Hotels.ng is changing the game <br> within the African tech space.</h4>
+
+<div class="deep">
+	<p><h4 class="head-text"><?php echo date("Y/m/d") ?>
+	 <br>
+<?php date_default_timezone_set('Africa/Lagos'); echo date("h:i:s a" , time()) ?></h4></p>
+
+</div>
+</body>
+</html>
